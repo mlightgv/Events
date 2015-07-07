31d662c71d7a9280239963ebd4f49fdbe47f3e82
Mary Gomez Villarreal <marygomez@192-168-1-9.tpgi.com.au>
2015-07-07 11:10:08 +1000

    create method search tags in model

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,10 @@ class Event < ActiveRecord::Base
   	friendly_id :title, use: :slugged
 	belongs_to :organizers, class_name: "User"
 
+	def self.tagged_with(name)
+		Tag.find_by_name!(name).events
+	end
+
 	def all_tags=(names)
 		self.tags = names.split(",").map do |n|
 			Tag.where(name: n.strip).first_or_create!
